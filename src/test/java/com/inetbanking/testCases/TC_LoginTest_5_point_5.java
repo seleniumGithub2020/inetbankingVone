11eb669c2cbc06822a3e027f711840b98ebd7089
seleniumGithub2020 <malesj2003@yahoo.com>
2021-09-08 18:10:04 -0700

    this is my first commit

---
+++ src/test/java/com/inetbanking/testCases/TC_LoginTest_5_point_5.java
@@ -0,0 +1,38 @@
+package com.inetbanking.testCases;
+
+import java.io.IOException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.inetbanking.pageObjects.LoginPage3;
+
+public class TC_LoginTest_5_point_5 extends BaseClass4
+{
+	
+	@Test 
+	public void loginTest() throws IOException
+	{
+		driver.get(baseURL);
+		logger.info("URL is opened");
+		LoginPage lp = new LoginPage(driver);
+		
+		lp.setUserName(username);
+		logger.info("Enter username");
+		
+		lp.setPassword(password);
+		logger.info("Enter password");
+		
+		lp.clickSubmit();
+		
+		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
+		{
+			captureScreen(driver,"loginTest");
+			Assert.assertTrue(true);
+			logger.info("Login test passed");
+		}
+		else
+		{
+			Assert.assertTrue(false);
+			logger.info("Login test failed");
+		}	
+	}
+}
